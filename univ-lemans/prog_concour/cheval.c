bb1ff2162503f0e5950d3ae022d4bfe1b7f34b88
Diallo Mamadou <s185313@ic2s120-11.univ-lemans.fr>
2018-09-25 12:05:15 +0200

    ajout tp prog_conco

---
+++ univ-lemans/prog_concour/cheval.c
@@ -0,0 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+#include <commun.h>
+#include <liste.h>
+#include <piste.h>
+
+int
+main( int nb_arg , char * tab_arg[] )
+{
+
+  int cle_piste ;
+  // piste_t * piste = NULL ;
+  piste_t * piste = (piste_t *)malloc( sizeof(piste_t) ) ;
+
+
+  int cle_liste ;
+  /* liste_t * liste = NULL ; */
+  liste_t * liste = (liste_t *)malloc( sizeof(liste_t) ) ;
+
+  // char marque;
+  char marque = 'A';
+
+  booleen_t fini = FAUX ;
+  piste_id_t deplacement = 0 ;
+  piste_id_t depart = 0 ;
+  piste_id_t arrivee = 0 ;
+
+
+  cell_t cell_cheval ;
+
+
+  elem_t elem_cheval ;
+
+  if( nb_arg != 4 )
+    {
+      fprintf( stderr, "usage : %s <cle de piste> <cle de liste> <marque>\n" , tab_arg[0] );
+      exit(-1);
+    }
+
+  if( sscanf( tab_arg[1] , "%d" , &cle_piste) != 1 )
+    {
+      fprintf( stderr, "%s : erreur , mauvaise cle de piste (%s)\n" ,
+	       tab_arg[0]  , tab_arg[1] );
+      exit(-2);
+    }
+
+
+  if( sscanf( tab_arg[2] , "%d" , &cle_liste) != 1 )
+    {
+      fprintf( stderr, "%s : erreur , mauvaise cle de liste (%s)\n" ,
+	       tab_arg[0]  , tab_arg[2] );
+      exit(-2);
+    }
+
+  if( sscanf( tab_arg[3] , "%c" , &marque) != 1 )
+    {
+      fprintf( stderr, "%s : erreur , mauvaise marque de cheval (%s)\n" ,
+	       tab_arg[0]  , tab_arg[3] );
+      exit(-2);
+    }
+
+
+  /* Init de l'attente */
+  commun_initialiser_attentes() ;
+
+
+  /* Init de la cellule du cheval pour faire la course */
+  cell_pid_affecter( &cell_cheval  , getpid());
+  cell_marque_affecter( &cell_cheval , marque );
+
+  /* Init de l'element du cheval pour l'enregistrement */
+  elem_cell_affecter(&elem_cheval , cell_cheval ) ;
+  elem_etat_affecter(&elem_cheval , EN_COURSE ) ;
+
+  /*
+   * Enregistrement du cheval dans la liste
+   */
+
+   // ...
+
+
+  while( ! fini )
+    {
+      /* Attente entre 2 coup de de */
+      commun_attendre_tour() ;
+
+      /*
+       * Verif si pas decanille
+       */
+
+      if(elem_decanille(elem_cheval)){
+        printf(" ----- est_decanillé \n");
+      }
+      else{
+        //...
+      }
+
+
+      /*
+       * Avancee sur la piste
+       */
+
+      /* Coup de de */
+      deplacement = commun_coup_de_de() ;
+#ifdef _DEBUG_
+      printf(" Lancement du De --> %d\n", deplacement );
+#endif
+
+      arrivee = depart+deplacement ;
+
+      if( arrivee > PISTE_LONGUEUR-1 )
+	{
+	  arrivee = PISTE_LONGUEUR-1 ;
+	  fini = VRAI ;
+	}
+
+      if( depart != arrivee )
+	{
+
+	  /*
+	   * Si case d'arrivee occupee alors on decanille le cheval existant
+	   */
+
+     if(piste_cell_occupee(piste,arrivee)){
+       int ind_rech;
+       if(liste_elem_rechercher(&ind_rech,liste,elem_cheval)){
+         liste_elem_decaniller(liste,ind_rech);
+       }
+       //changement de seamphore à faire
+     }
+
+	  /*
+	   * Deplacement: effacement case de depart, affectation case d'arrivee
+	   */
+
+     piste_cell_effacer(piste,depart);
+     piste_cell_affecter(piste,arrivee,cell_cheval);
+
+#ifdef _DEBUG_
+	  printf("Deplacement du cheval \"%c\" de %d a %d\n",
+		 marque, depart, arrivee );
+#endif
+
+
+	}
+      /* Affichage de la piste  */
+      piste_afficher_lig( piste );
+
+      depart = arrivee ;
+    }
+
+  printf( "Le cheval \"%c\" A FRANCHIT LA LIGNE D ARRIVEE\n" , marque );
+
+
+
+  /*
+   * Suppression du cheval de la liste
+   */
+   int ind_suppr;
+   if(liste_elem_rechercher(&ind_suppr,liste,elem_cheval)){
+     liste_elem_supprimer(liste,ind_suppr);
+   }
+
+  exit(0);
+}
