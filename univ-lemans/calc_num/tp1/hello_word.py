02bb4138e4ec676eb6568893d1adee8a3d582490
Diallo Mamadou <s185313@ic2s122-08.univ-lemans.fr>
2018-10-26 13:26:18 +0200

    fin tp1 calc num

--- univ-lemans/calc_num/tp1/hello_word.py
+++ univ-lemans/calc_num/tp1/hello_word.py
@@ -1,14 +1,7 @@
-# f = open("synpaflex-pronunciation-dictionary.txt","r",encoding="utf8")
-# numLigne = input("entrez le numero de ligne ")
-# for i in range(int(numLigne)):
-#     res = f.readline()
-#     print(res.strip("\n"))
-# f.close()
-
 def AfficherLigne(line):
     print(line.strip("\n"))
 
-def StockerLigne(line, dictPron):
+def StockerLigne(line, dicPron):
     mot = ""
     val = ""
     i = 0
@@ -21,23 +14,28 @@ def StockerLigne(line, dictPron):
     while(i+1 != len(line)):
         val += line[i+1]
         i += 1;
-    # print(mot)
-    val = val.strip("\n")
-    # print(val)
-    dictPron[mot] = val
-    print(dictPron)
+    dicPron[mot] = val.strip("\n")
+
+def AfficherPron(m, dicPron):
+    print(dicPron.get(m))
+
+def AfficherMots(p, dicPron):
+    liste = []
+    for mot,pron in dicPron.items():
+        if pron == p:
+            liste.append(mot)
+    print("il y a ",len(liste)," mot(s) de prononciation ","/",p,"/ qui sont :")
+    for mot in liste:
+        print(mot)
 
 def main():
     f = open("synpaflex-pronunciation-dictionary.txt","r",encoding="utf8")
-    numLigne = input("entrez le numero de ligne ")
-    for i in range(int(numLigne)):
-        res = f.readline()
-        # print(res.strip("\n"))
-    dictPron = dict()
-    preLigne = f.readline()
-    # AfficherLigne(preLigne)
-    StockerLigne(res, dictPron)
-
+    lignes = f.readlines()
+    dicPron = dict()
+    for ligne in lignes:
+        StockerLigne(ligne, dicPron)
+    AfficherMots('k u l e',dicPron)
+    AfficherMots('a b ɛ s ɛ',dicPron)
     f.close()
 
 if __name__ == "__main__":
